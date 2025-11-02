5b5833417adeb0dd88db625697e9cf2d2f0eea84
David Landry <13077061+David3141@users.noreply.github.com>
2025-11-02 22:07:18 +0100

    Add <leader>cn and <leader>cN to change next/prev occurences

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -17,3 +17,7 @@ vim.keymap.set("n", "<leader>k", ":m .-2<CR>==")
 vim.keymap.set("n", "x", '"_x', { noremap = true, silent = true })
 vim.keymap.set("n", "X", '"_X', { noremap = true, silent = true })
 vim.keymap.set("v", "x", '"_x', { noremap = true, silent = true })
+
+-- Change current word in a repeatable manner
+vim.keymap.set("n", "<leader>cn", "*``cgn", { noremap = true, silent = true, desc = "Change next occurence" })
+vim.keymap.set("n", "<leader>cN", "*``cgN", { noremap = true, silent = true, desc = "Change previous occurence" })
