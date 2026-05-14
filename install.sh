47396bf1eb4c91910d50c04f692c22c9b5ee04eb
David Landry <david.landry@finstreet.de>
2026-05-14 13:19:14 +0200

    Add new install script that installs nvim

--- install.sh
+++ install.sh
@@ -1,11 +1,28 @@
 #!/usr/bin/env bash
 
 # Install script for devpod
-echo "TEST"
+function install_nvim_tarball() {
+  nvim_download_path=/tmp/nvim-linux-arm64.tar.gz
+  nvim_target=/opt/nvim
 
-brew install stow neovim fd ripgrep lazygit
+  curl -Lo "$nvim_download_path" https://github.com/neovim/neovim/releases/download/v0.12.2/nvim-linux-arm64.tar.gz
 
-stow nvim
-stow zsh
+  sudo rm -rf "${nvim_target}"
+  sudo mkdir -p "${nvim_target}"
+  sudo tar -C "${nvim_target}" --strip-components=1 -xzf "$nvim_download_path"
+  rm -rf "$nvim_download_path"
 
-echo "INSTALLED"
+  if ! "${nvim_target}/bin/nvim" --version; then
+    echo "nvim binary in ${nvim_target}/bin cannot be opened. Install from nvim tarball failed"
+    sudo rm -rf "${nvim_target}"
+    return 1
+  fi
+  if ! grep -Fxq "export PATH=\"\$PATH:${nvim_target}/bin\"" "$HOME/.zshrc"; then
+    echo "export PATH=\"\$PATH:${nvim_target}/bin\"" >>$HOME/.zshrc
+  fi
+
+  export PATH="$PATH:${nvim_target}/bin"
+  echo "INSTALLED nvim"
+}
+
+install_nvim_tarball
