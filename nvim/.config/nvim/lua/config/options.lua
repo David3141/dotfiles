c1fab1c2d6f8211c9d7cda53dba7f8b7b8176351
David Landry <david.landry@finstreet.de>
2026-05-17 13:34:18 +0200

    Use host clipboard from within ssh'd devcontainer in nvim

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -7,3 +7,24 @@ opt.scrolloff = 10
 
 -- Acoid conflicts between Biome and Prettier
 vim.g.lazyvim_prettier_needs_config = true
+
+local function in_container()
+  return vim.env.REMOTE_CONTAINERS == "true" -- set by VS Code / devpod devcontainers
+    or vim.env.CODESPACES == "true" -- GitHub Codespaces, just in case
+    or vim.uv.fs_stat("/.dockerenv") ~= nil -- generic Docker fallback
+end
+
+-- Use system clipboard from within devcontainers
+if in_container() then
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    },
+  }
+end
