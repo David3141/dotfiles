c16befd6992f9e7d75efef9b9ab1ec53480638d5
David Landry <david.landry@finstreet.de>
2026-05-17 13:49:16 +0200

    Let ruby-lsp handle rubocop and disable rubocop for mason

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -1,11 +1,17 @@
 return {
-    "neovim/nvim-lspconfig",
-    opts = {
-        diagnostics = {
-            virtual_text = true,
-        },
-        inlay_hints = {
-            enabled = false,
-        },
+  "neovim/nvim-lspconfig",
+  opts = {
+    diagnostics = {
+      virtual_text = true,
     },
+    inlay_hints = {
+      enabled = false,
+    },
+    servers = {
+      rubocop = {
+        enabled = false,
+        mason = false,
+      },
+    },
+  },
 }
